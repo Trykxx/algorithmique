9462822b66762827eb724482e839f81d250162b2
Trykxx <amitnicolas@gmail.com>
2024-02-14 17:01:44 +0100

    main

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@
 // Afficher des informations dans la console de debbugage du navigateur 
 console.log('Hello world')
 
+// Afficher une boite de dialogue avec champs a remplir 
+// let response = prompt('Quel est votre age ?')
+// console.log(response)
+
 /*---------------------- Les variables --------------------------*/
 
 // Declaration d'une variable
@@ -46,9 +50,6 @@ sum = number1 * number2
 console.log(sum)
 
 
-
-
-
 /*---------------------- La Concatenation --------------------------*/
 
 let firstName ='Amit Nicolas' 
@@ -67,10 +68,173 @@ console.log("J'adore vivre à " + city)
 console.log('J\'adore vivre à ' + city)
 
 // Modifier le message pour afficher j'adore vivre à <city> et je m'appelle <firstname> <lastname>
- 
+
 console.log(`J'adore vivre à ${city} et je m'appelle ${firstName} ${lastName}`)
 
 
+/*---------------------- Instructions Conditionnelles --------------------------*/
+
+// Demander a l'utilisateur si il accepte les cgv
+
+//let agree= prompt ('Accepter vous les cgv ?')
+//console.log(agree)
+
+/*if(agree == 'ok'){
+    console.log('Vous avez accepté')
+} else { 
+    console.log('Vous n\'avez pas accepté')
+}
+
+// Si, Si non Si, Sinon (if, else if, else)
+// Si la condition est vraie le premier bloc d'instruction est éxécuté
+// Sinon il test la deuxième condition
+// Sinon aucune condition n'est vraie, il éxécute le bloc final
+
+
+// si il repond yes
+// vous etes redirigé vers le site en anglais
+// Si il repond oui
+// vous etes redirigé vers le site en français
+// Si il repond da
+// vous etes redirigé vers le site en russe
+// Sinon
+// vous ne pouvez accéder au site
+if(agree == 'yes'){
+    console.log("vous etes redirigé vers le site en anglais")
+    
+}else if(agree== "oui"){
+    console.log("vous etes redirigé vers le site en français")
+    
+}else if(agree == "da"){
+    console.log("vous etes redirigé vers le site en russe")
+}else {
+    console.log("vous ne pouvez accéder au site")
+}
 
 
 
+console.log('La suite...')
+
+/*---------------------- Les opérateurs de comparaison --------------------------*/
+
+// ==, >, >=, <, <=, !=, ===
+// Déclarer une variable qui s'appelle variable, lui assigner la valeur 16
+// Si la variable est égale à 16 afficher félicitations
+
+let variable= 16;
+
+if (variable = 16) {
+    console.log("droit de conduire")
+    
+}
+
+/* Ecrire un algorithme qui demande a l'utilisateur son age. Il 
+indique ensuite a l'utilisateur quel film il peut aller voir
+*/
+//"Action man" si moins de 13 ans
+//"Matrix" si il a entre 13 et 18 ans 
+//"Evil dead" si il a plus de 18 ans 
+
+//let age= prompt ('Quel age avez-vous ?')
+
+/*if (age < 13) {
+    console.log("Action man")
+}else if (age >= 13 && age <= 18) {
+    console.log("Matrix")
+}else if (age > 18) {
+    console.log("Evil dead")
+}
+
+// Demander a l'utilisateur son age 
+// Demander sa nationalité
+
+// Si il est français et qu'il a plus de 18 ans alors OU s'il est étranger et qu'il a moins de 18ans
+// Assistance juridique gratuite 
+*/
+
+//     let age= prompt ('Quel age avez-vous ?')
+//     let nat= prompt ("Quelle est votre nationalité ?")
+
+
+//    if (
+    //     (nat == "français" && age >= 18) || 
+    //     (nat != "français" && age < 18 )
+    //     ) {
+        //        console.log("Assistance juridique gratuite")
+        //    }
+        
+        // Demande sa nationalite
+        // Si il est français
+        //demandez a l'utilisateur son age 
+        //si il a plus de 18 ans 
+        //droit aux APL
+        
+        //sinon
+        //vous n'avez aucun droit 
+        
+        // let nat = prompt ("Quelle est votre nationalite ?");
+        
+        
+    //     if (nat =="français") {
+    //         let age= prompt("Quel est votre age ?")
+    //         if (age >= 18){
+    //             console.log("Vous avez droit aux APL")
+                
+    //         }else{
+    //             console.log("Vous n'avez aucun droit")
+    //         }
+            
+    //     }else {
+    //     console.log("Vous avez aucun droit")
+    // }
+    
+    /*---------------------- Les opérateurs de comparaison --------------------------*/
+    
+    // Les boucles sont des structures permettant d'éxécuter plusieurs fois des instructions
+
+    // Boucle a itération non définie (while)
+    let i = 0;
+    while( i < 9 ) {
+        i = i + 1;
+        console.log(i);
+    }
+        
+
+    // Afficher la table de multiplication de 7
+    
+    let y = 0;
+    while( y <= 10){
+        let res = y * 7; 
+        console.log(`${y} x 7 = ${res}`);
+        y = y + 1
+        
+    }
+    
+    // Ecrire un algorithme qui inverse les valeurs de deux variables A et B quel que soit le contenu de A et B
+    
+    let a = 12;
+    let b = 14;
+    console.log(a, b)
+    let temp = a;
+    a = b;
+    b = temp
+    console.log(a, b)
+    
+    
+
+
+
+
+
+        
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
