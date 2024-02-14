67c760bf7a678889fd66c2c3b93b56a19a7c6ee7
Trykxx <amitnicolas@gmail.com>
2024-02-14 20:51:03 +0100

    update algorithme

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-/*---------------------- Les commentaires --------------------------*/
+/* --------------------------- Les Commentaires --------------------------- */
+
+// Un commentaire est un texte qui ne sera pas exécuté par le navigateur
+// Il permet de documenter le code, de le rendre plus lisible
+// Il permet de désactiver une partie du code
 
 // Un commentaire en Javascript
 
@@ -7,234 +11,206 @@
     en Javascript
 */
 
-/*---------------------- Affichage --------------------------*/
+/* --------------------------- Affichage --------------------------- */
 
-// Afficher des informations dans la console de debbugage du navigateur 
-console.log('Hello world')
+// Afficher un message dans la console de debbugage du navigateur
+// On utilise la fonction console.log()
+console.log('Hello, world!');
 
-// Afficher une boite de dialogue avec champs a remplir 
-// let response = prompt('Quel est votre age ?')
-// console.log(response)
+// Afficher une boite de dialogue avec champs à remplir
+// On utilise la fonction prompt()
+// prompt('Quel est votre age?');
 
-/*---------------------- Les variables --------------------------*/
+// On doit récupérer dans une variable la valeur du prompt
 
-// Declaration d'une variable
+// let response = prompt('Quel est votre age???');
+// console.log(response);
+
+/* --------------------------- Les Variables --------------------------- */
+
+// Les variables ne peuvent pas commencer par un chiffre
+// Les variables ne peuvent pas contenir de caractères spéciaux (-, +, *, /, %, $, @, &, ...)
+// Les variables ne peuvent pas contenir d'espaces
+
+// Déclaration d'une variable
 let uneVariable;
 
-// Affectation d'une variable 
-uneVariable = 'Je suis une variable!'
+// Affectation d'une valeur à une variable
+uneVariable = 'Je suis une variable!';
 
-// Les 2 se font généralement en meme temps
-// Les variables sont sensibles à la casse 
-let helloworld = 'Hello, world';
+// Les 2 se font généralement en même temps
+// Les variables sont sensibles à la casse (majuscules =/= minuscules )
+let messageBonjour = 'Hello, World!';
+let MESSAGEBONJOUR;
 
-uneVariable ='Je suis une nouvelle variable'
+// On peut affecter le contenu d'une variable à une autre variable
+messageBonjour = uneVariable;
 
-// Les différents types de variables
-let messages ='un message'
-let number = 20
-let decimal = 20.5
-let boolean = true
-let boolean2 = false
+// Affichage d'une variable dans la console
+console.log(messageBonjour);
+
+// On peut changer la valeur d'une variable
+uneVariable = 'Je suis une nouvelle variable!';
+
+/* --------------------------- Les Types de Variables --------------------------- */
+
+// Les types les plus courants sont :
+
+// string (chaine de caractère)
+let messages = 'une chaine de caractère';
+
+// number (nombre)
+let nombre = 20;
+let nombreDecimal = 20.5;
+
+// boolean (booléen)
+let isTrue = true;
+let isFalse = false;
 
 // Les opérations arithmétiques (+, -, *, /)
-// Declarer 3 variables sum, number1, number2
-// Affecter le nombre 10 à number1
-// Affecter le nombre 20 à number2
-// Affecter à sum l'opération de multiplication de multiplication des 2 variables précédentes
+let sum;
+let number1 = 10;
+let number2 = 20;
+sum = number1 * number2;
+console.log(sum);
 
-let sum 
-let number1 = 10
-let number2 = 20
-sum = number1 * number2
-console.log(sum)
+/* --------------------------- La Concatenation --------------------------- */
 
+let lastName = 'EDJEKOUANE';
+let firstName = 'Rachid';
 
-/*---------------------- La Concatenation --------------------------*/
+// Pour déclarer une chaine de caractère on peux utiliser ('', "", ``)
+// Cela a un impact sur la façon de concatener les variables et les chaines de caractères
+let city = 'PARIS';
+city = 'PARIS';
+city = `PARIS`;
 
-let firstName ='Amit Nicolas' 
-let lastName ='Roig Menda'
+// Déclarer une variable city, afficher le message "j'adore vivre à <city>"
+console.log("j'adore vivre à " + city);
+console.log("j'adore vivre à " + city);
+console.log(`j'adore vivre à ${city}`);
 
-//Je m'appelle first name + lastname
-console.log("Je m'appelle " + firstName + ' ' + lastName)
+// Modifier le message pour afficher j'adore vivre à <city> et je m'appelle <firstName> <lastName>
+console.log(`j'adore vivre à ${city} et je m'appelle ${firstName} ${lastName}`);
 
-// Déclarer une variable city, afficher le message j'adore vivre a city 
+/* --------------------------- Instructions Conditionnelles --------------------------- */
 
-let city ='Tremblay'
-city =`Tremblay`
-city ="Tremblay"
+// Demander à l'utilisateur s'il accepte les CGV
+// let acceptCgv = prompt('Acceptez-vous les CGV?');
+let acceptCgv = 'oui';
 
-console.log("J'adore vivre à " + city)
-console.log('J\'adore vivre à ' + city)
-
-// Modifier le message pour afficher j'adore vivre à <city> et je m'appelle <firstname> <lastname>
-
-console.log(`J'adore vivre à ${city} et je m'appelle ${firstName} ${lastName}`)
-
-
-/*---------------------- Instructions Conditionnelles --------------------------*/
-
-// Demander a l'utilisateur si il accepte les cgv
-
-//let agree= prompt ('Accepter vous les cgv ?')
-//console.log(agree)
-
-/*if(agree == 'ok'){
-    console.log('Vous avez accepté')
-} else { 
-    console.log('Vous n\'avez pas accepté')
+// Si, Sinon (if, else)
+if (acceptCgv == 'ok') {
+    console.log('Vous avez acceptez les conditions!');
+} else {
+    console.log("Vous n'avez pas acceptez les conditions!");
 }
 
-// Si, Si non Si, Sinon (if, else if, else)
-// Si la condition est vraie le premier bloc d'instruction est éxécuté
+// Si, Si Non Si, Sinon (if, else if, else)
+// Si la condition est vraie le premier bloc d'instruction est exécuté
 // Sinon il test la deuxième condition
-// Sinon aucune condition n'est vraie, il éxécute le bloc final
-
-
-// si il repond yes
-// vous etes redirigé vers le site en anglais
-// Si il repond oui
-// vous etes redirigé vers le site en français
-// Si il repond da
-// vous etes redirigé vers le site en russe
-// Sinon
-// vous ne pouvez accéder au site
-if(agree == 'yes'){
-    console.log("vous etes redirigé vers le site en anglais")
-    
-}else if(agree== "oui"){
-    console.log("vous etes redirigé vers le site en français")
-    
-}else if(agree == "da"){
-    console.log("vous etes redirigé vers le site en russe")
-}else {
-    console.log("vous ne pouvez accéder au site")
+// Sinon aucune condition n'est vraie il exécute le bloc final
+if (acceptCgv == 'yes') {
+    console.log('Vous etes redirigé vers le site en anglais');
+} else if (acceptCgv == 'oui') {
+    console.log('Vous etes redirigé vers le site en français');
+} else if (acceptCgv == 'da') {
+    console.log('Vous etes redirigé vers le site en russe');
+} else {
+    console.log('Vous ne pouvez acceder au site');
 }
 
+console.log('la suite...');
 
+/* --------------------------- Les Opérateurs de Comparaison --------------------------- */
 
-console.log('La suite...')
+// Les opérateurs de comparaison permettent de comparer deux valeurs
+// Ils renvoient un résultat booléen (true, false)
 
-/*---------------------- Les opérateurs de comparaison --------------------------*/
+// ( ==, >, >=, <, <=, !=, ===) :
 
-// ==, >, >=, <, <=, !=, ===
-// Déclarer une variable qui s'appelle variable, lui assigner la valeur 16
-// Si la variable est égale à 16 afficher félicitations
+// (> ou >=) supérieur ou supérieur ou égal à
+// (< ou <=) inférieur ou inférieur ou égal à
+// (!=) différent de
+// (===) strictement égal à
+// (!==) strictement différent de
 
-let variable= 16;
+// (==) égal à
+let x = 10;
+let y = 10;
 
-if (variable = 16) {
-    console.log("droit de conduire")
-    
+console.log(a == b);
+
+/* --------------------------- Les Opérateurs Logiques --------------------------- */
+
+// Les opérateurs logiques permettent de combiner plusieurs conditions
+
+// ET (&&) : les deux conditions doivent être vraies
+// OU (||) : au moins une des conditions doit être vraie
+
+let condition = true;
+
+if (condition == true) {
+    console.log('droit de conduire');
 }
 
-/* Ecrire un algorithme qui demande a l'utilisateur son age. Il 
-indique ensuite a l'utilisateur quel film il peut aller voir
-*/
-//"Action man" si moins de 13 ans
-//"Matrix" si il a entre 13 et 18 ans 
-//"Evil dead" si il a plus de 18 ans 
+// let age = prompt('Quel est ton age?');
+let userAge = 19;
 
-//let age= prompt ('Quel age avez-vous ?')
-
-/*if (age < 13) {
-    console.log("Action man")
-}else if (age >= 13 && age <= 18) {
-    console.log("Matrix")
-}else if (age > 18) {
-    console.log("Evil dead")
+if (userAge < '13') {
+    console.log('Action Man');
+} else if (userAge >= '13' || userAge <= 18) {
+    console.log('Matrix');
+} else if (userAge > 18) {
+    console.log('Evil Dead');
 }
 
-// Demander a l'utilisateur son age 
-// Demander sa nationalité
+// let age = prompt('Quel est ton age?');
+// let nationalite = prompt('Quelle est votre nationalité');
 
-// Si il est français et qu'il a plus de 18 ans alors OU s'il est étranger et qu'il a moins de 18ans
-// Assistance juridique gratuite 
-*/
+let userAge2 = 19;
+let nationality = 'français';
 
-//     let age= prompt ('Quel age avez-vous ?')
-//     let nat= prompt ("Quelle est votre nationalité ?")
+if (
+    (nationality == 'français' && userAge2 >= 18) ||
+    (nationality != 'français' && userAge2 < 18)
+) {
+    console.log('Vous avez le droit à une assistance juridique');
+}
 
+// let nationality2 = prompt('Quelle est votre nationalité?');
+let nationality2 = 'anglais';
+console.log(nationality2);
 
-//    if (
-    //     (nat == "français" && age >= 18) || 
-    //     (nat != "français" && age < 18 )
-    //     ) {
-        //        console.log("Assistance juridique gratuite")
-        //    }
-        
-        // Demande sa nationalite
-        // Si il est français
-        //demandez a l'utilisateur son age 
-        //si il a plus de 18 ans 
-        //droit aux APL
-        
-        //sinon
-        //vous n'avez aucun droit 
-        
-        // let nat = prompt ("Quelle est votre nationalite ?");
-        
-        
-    //     if (nat =="français") {
-    //         let age= prompt("Quel est votre age ?")
-    //         if (age >= 18){
-    //             console.log("Vous avez droit aux APL")
-                
-    //         }else{
-    //             console.log("Vous n'avez aucun droit")
-    //         }
-            
-    //     }else {
-    //     console.log("Vous avez aucun droit")
-    // }
-    
-    /*---------------------- Les opérateurs de comparaison --------------------------*/
-    
-    // Les boucles sont des structures permettant d'éxécuter plusieurs fois des instructions
-
-    // Boucle a itération non définie (while)
-    let i = 0;
-    while( i < 9 ) {
-        i = i + 1;
-        console.log(i);
+if (nationality2 == 'français') {
+    let age = prompt('Quel est votre age?');
+    if (age >= 18) {
+        console.log('Vous avez droit aux APL');
+    } else {
+        console.log("Vous n'avez aucun droit!");
     }
-        
+} else {
+    console.log("Vous n'avez aucun droit");
+}
 
-    // Afficher la table de multiplication de 7
-    
-    let y = 0;
-    while( y <= 10){
-        let res = y * 7; 
-        console.log(`${y} x 7 = ${res}`);
-        y = y + 1
-        
-    }
-    
-    // Ecrire un algorithme qui inverse les valeurs de deux variables A et B quel que soit le contenu de A et B
-    
-    let a = 12;
-    let b = 14;
-    console.log(a, b)
-    let temp = a;
-    a = b;
-    b = temp
-    console.log(a, b)
-    
-    
+/* --------------------------- Les Boucles --------------------------- */
 
+// Les boucles sont des structures permettant d'executer plusieurs fois des instructions
+// Il existe plusieurs types de boucles en Javascript
 
+// Boucle à itération non definies (while) : tant que la condition est vraie
+let i = 0;
+while (i < 10) {
+    let res = i * 7;
+    console.log(`${i} x 7 = ${res}`);
+    i = i + 1; // Incrementer i pour éviter une boucle infinie
+}
 
+// Ecrire un algorithme qui inverse les valeurs de deux variables A et B quel que soit le contenu de A et de B
+let a = 10;
+let b = 99;
 
-
-        
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
